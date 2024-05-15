68917d44f1849f9480c6afbcf68d5cfea6a03c9d
ShivendraSinghThakur <91961373+ShivendraSinghThakur@users.noreply.github.com>
2024-05-15 14:51:51 +0530

    Update OrdersDao.java

--- ECom/src/main/java/com/java/ECom/dao/OrdersDao.java
+++ ECom/src/main/java/com/java/ECom/dao/OrdersDao.java
@@ -8,4 +8,9 @@ import com.java.ECom.model.Orders;
 public interface OrdersDao {
 
 	List<Orders> showCustomerOrders(int custId) throws ClassNotFoundException, SQLException;
+
+	String PlaceOrder(int Cust_Id, int Product_Id, int QuantityOrd) throws ClassNotFoundException, SQLException;
+
+	int generateOrderId() throws ClassNotFoundException, SQLException;
+}
 }
